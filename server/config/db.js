caa599ea37618e5936a51dd569bd3744b7a85782
Theresa <tbenne21@my.centennialcollege.ca>
2023-07-12 21:56:31 -0400

    survey page & survey creation details

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,5 @@
-// File name: Express app portfolio
-// Student’s Name: Theresa Bennett
-// StudentID: 300909345
-// Date: June 18th, 2023
 
 module.exports = 
 {
-    "URI": "mongodb+srv://Tbennett2:IUouioZiI2qR1Rq3@mongodbserver2.magwtoj.mongodb.net/contact_list?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://Tbennett2:IUouioZiI2qR1Rq3@mongodbserver2.magwtoj.mongodb.net/contact_list?retryWrites=true&w=majorit"
 }
