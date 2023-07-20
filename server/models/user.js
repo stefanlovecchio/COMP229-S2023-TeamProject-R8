183456c11ae871444f3c94adad551845887c4cf1
stefanlovecchio <99218647+stefanlovecchio@users.noreply.github.com>
2023-07-20 13:15:59 -0400

    fixed merge conflicts

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 //require modules for the User Module
 let mongoose = require('mongoose');
 let passportLocalMongoose = require('passport-local-mongoose');
@@ -59,40 +58,3 @@ User.plugin(passportLocalMongoose, options);
 
 module.exports.User = mongoose.model('User', User);
 
-=======
-let mongoose = require ('mongoose');
-let passportLocalMongoose = require('passport-local-mongoose');
-
-let User = mongoose.Schema(
-{
-    username:
-    {
-        type:String,
-        default:"",
-        trim:true,
-        require:"Username is required."
-    },
-    email:
-    {
-        type:String,
-        default:"",
-        trim:true,
-        require:"Email address is required."
-    },
-    displayName:
-    {
-        type:String,
-        default:"",
-        trim:true,
-        require:"Display name is required."
-    }
-},
-{
-    collection:'users'
-}
-)
-//configure option for user model
-let options = ({missingPasswordError:"Wring / Missing Password "});
-User.plugin(passportLocalMongoose, options);
-module.exports.User = mongoose.model('User',User);
->>>>>>> Stashed changes
